f068fb80308628765b4ced1d7322845237c67240
Varun Nagaraj <varun.n.nagaraj@oracle.com>
2025-07-02 20:42:36 -0700

    Update google sheets storage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -325,6 +325,7 @@ const Index = () => {
           </Button>
         </div>
 
+        {/* Add/Edit Form */}
         {showAddForm && (
           <Card className="mb-6 shadow-lg rounded-2xl border-0">
             <CardHeader className="pb-4">
